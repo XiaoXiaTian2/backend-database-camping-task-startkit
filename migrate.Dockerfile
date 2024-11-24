d4c2da18eb533a17e1c9a2eba5ef5a319aa7b119
XiaoXiaTian2 <s59444997880@gmail.com>
2024-11-24 16:27:26 +0800

    修改docker config、新增sql

--- migrate.Dockerfile
+++ migrate.Dockerfile
@@ -1,4 +1,4 @@
- FROM node:20-alpine3.19
+ FROM node:22.10.0-alpine3.19
  WORKDIR /
  COPY . /
  RUN npm ci --production
--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -6,6 +6,14 @@
 --   █ █   █████ █   █     █ 
 -- ===================== ====================
 -- 1. 用戶資料，資料表為 USER
+CREATE TABLE IF NOT EXISTS "USER" (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+  "name" varchar(50) NOT NULL,
+  "email" varchar(320) UNIQUE NOT NULL,
+  "role" varchar(20) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
 -- 1. 新增：新增六筆用戶資料，資料如下：
 --     1. 用戶名稱為`李燕容`，Email 為`lee2000@hexschooltest.io`，Role為`USER`
@@ -13,16 +21,37 @@
 --     3. 用戶名稱為`肌肉棒子`，Email 為`muscle@hexschooltest.io`，Role為`USER`
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
---     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
+--     6. 用戶名稱為`透明人`，Email 為 opacity0@hexschooltest.io，Role 為 USER
+INSERT INTO "USER" (name, email, role)
+VALUES
+  ('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+  ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
+  ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
+  ('好野人', 'richman@hexschooltest.io', 'USER'),
+  ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
+  ('透明人', 'opacity0@hexschooltest.io', 'USER')
+;
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
+UPDATE "USER"
+SET role = 'COACH'
+WHERE 
+    email IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
+    AND 
+    role = 'USER';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
+DELETE FORM "USER"
+WHERE email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
+SELECT COUNT(*) AS 所有用戶數量
+FROM USER;
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
-
+SELECT *
+FROM USER
+LIMIT 3;
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
@@ -31,16 +60,85 @@
 --    █ █   █████ █   █    █████ 
 -- ===================== ====================
 -- 2. 組合包方案 CREDIT_PACKAGE、客戶購買課程堂數 CREDIT_PURCHASE
+CREATE TABLE IF NOT EXISTS "CREDIT_PACKAGE" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(50) NOT NULL,
+  "cover_url" varchar(2048),
+  "credit_amount" integer NOT NULL,
+  "price" numeric(10,2) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE IF NOT EXISTS IF NOT EXISTS "CREDIT_PURCHASE" (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+  "user_id" uuid NOT NULL REFERENCES "USER"(id),
+  "credit_package_id" integer NOT NULL REFERENCES "CREDIT_PACKAGE"(id),
+  "purchased_credits" integer NOT NULL,
+  "price_paid" numeric(10,2) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  "purchase_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
 -- 2-1. 新增：在`CREDIT_PACKAGE` 資料表新增三筆資料，資料需求如下：
     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
+INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price)
+VALUES 
+  ('7 堂組合包方案', 7, 1400),
+  ('14 堂組合包方案', 14, 2520),
+  ('21 堂組合包方案', 21, 4800)
+;
 
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid)
+VALUES
+  (
+    (SELECT id FROM USER WHERE name = '王小明'),
+    (SELECT id FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+    (SELECT credit_amount FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+    (SELECT price FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+  ),
+  (
+    (SELECT id FROM USER WHERE name = '王小明'),
+    (SELECT id FROM CREDIT_PACKAGE WHERE name = '21 堂組合包方案'),
+    (SELECT credit_amount FROM CREDIT_PACKAGE WHERE name = '21 堂組合包方案'),
+    (SELECT price FROM CREDIT_PACKAGE WHERE name = '21 堂組合包方案'),
+  ),
+  (
+    (SELECT id FROM USER WHERE name = '好野人'),
+    (SELECT id FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+    (SELECT credit_amount FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+    (SELECT price FROM CREDIT_PACKAGE WHERE name = '14 堂組合包方案'),
+  )
+;
+-- 優化版
+WITH User AS (
+  SELECT id AS user_id, name 
+  FROM "USER"
+  WHERE name IN ('王小明', '好野人')
+),
+Package AS (
+  SELECT id AS package_id, name, credit_amount, price
+  FROM "CREDIT_PACKAGE"
+  WHERE name IN ('14 堂組合包方案', '21 堂組合包方案')
+)
 
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid)
+SELECT 
+  u.user_id,
+  p.package_id,
+  p.credit_amount,
+  p.price
+FROM User u
+JOIN Package p
+ON 
+  (u.name = '王小明' AND p.name IN ('14 堂組合包方案', '21 堂組合包方案'))
+  OR
+  (u.name = '好野人' AND p.name = '14 堂組合包方案');
 
 -- ████████  █████   █    ████   
 --   █ █   ██    █  █         ██ 
@@ -49,22 +147,133 @@
 --   █ █   █████ █   █    ████   
 -- ===================== ====================
 -- 3. 教練資料 ，資料表為 COACH ,SKILL,COACH_LINK_SKILL
+CREATE TABLE IF NOT EXISTS "SKILL" (
+  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "name" varchar(50) UNIQUE NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE IF NOT EXISTS "COACH" (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "user_id" uuid NOT NULL REFERENCES "USER"(id),
+  "experience_years" integer,
+  "description" text,
+  "profile_image_url" varchar(2048),
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  UNIQUE("user_id")
+);
+
+CREATE TABLE IF NOT EXISTS "COACH_LINK_SKILL" (
+  "coach_id" uuid NOT NULL REFERENCES "COACH"(id),
+  "skill_id" uuid NOT NULL REFERENCES "SKILL"(id),
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  PRIMARY KEY ("coach_id", "skill_id")
+);
 -- 3-1 新增：在`COACH`資料表新增三筆教練資料，資料需求如下：
     -- 1. 將用戶`李燕容`新增為教練，並且年資設定為2年（提示：使用`李燕容`的email ，取得 `李燕容` 的 `id` ）
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
+INSERT INTO "COACH" (user_id, experience_years)
+VALUES
+  (
+    (SELECT id FROM "USER" WHERE name = '李燕容'),
+    2
+  ),
+  (
+    (SELECT id FROM "USER" WHERE name = '肌肉棒子'),
+    2
+  ),
+  (
+    (SELECT id FROM "USER" WHERE name = 'Q太郎'),
+    2
+  )
+;
 
--- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
+-- WITH優化版
+WITH "CUSER" AS (
+  SELECT id, name
+  FROM "USER"
+  WHERE name IN ('李燕容', '肌肉棒子', 'Q太郎')
+)
+INSERT INTO "COACH" (user_id, experience_years)
+SELECT 
+  u.id, 
+  2
+FROM "CUSER" u
+WHERE u.name IN ('李燕容', '肌肉棒子', 'Q太郎');
+
+    -- JOIN優化版
+INSERT INTO "COACH" (user_id, experience_years)
+SELECT u.id, 2
+FROM "USER" u
+JOIN (
+    VALUES
+        ('李燕容'),
+        ('肌肉棒子'),
+        ('Q太郎')
+) AS new_coaches(name)
+ON u.name = new_coaches.name;
+
+--? 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜珈` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
 
+-- 1.
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+SELECT
+  c.id AS coach_id,
+  s.id AS skill_id
+FROM "COACH" c
+CROSS JOIN (SELECT id FROM "SKILL" WHERE name = '重訓') s;
+
+-- 2.
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+SELECT 
+    c.id AS coach_id,
+    s.id AS skill_id
+FROM 
+    "COACH" c
+JOIN 
+    "USER" u ON c.user_id = u.id
+JOIN 
+    "SKILL" s ON s.name = '瑜伽'
+WHERE 
+    u.name = '肌肉棒子';
+
+-- 3.
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id)
+SELECT 
+    c.id AS coach_id,
+    s.id AS skill_id
+FROM 
+    "COACH" c
+JOIN 
+    "USER" u ON c.user_id = u.id
+JOIN 
+    "SKILL" s ON s.name IN ('有氧運動', '復健訓練')
+WHERE 
+    u.name = 'Q太郎';
+
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
+UPDATE "COACH"
+SET experience_years = 
+  CASE
+    WHEN name = '肌肉棒子' THEN 3
+    WHEN name = 'Q太郎' THEN 5
+    ELSE experience_years -- 保持原值不變
+  END;
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+INSERT INTO "SKILL" (name)
+VALUES
+  ('空中瑜伽');
 
+DELETE FROM "SKILL"
+WHERE name = '空中瑜伽';
 
 --  ████████  █████   █    █   █ 
 --    █ █   ██    █  █     █   █ 
@@ -73,6 +282,27 @@
 --    █ █   █████ █   █        █ 
 -- ===================== ==================== 
 -- 4. 課程管理 COURSE 、組合包方案 CREDIT_PACKAGE
+CREATE TABLE "COURSE" (
+  "id" serial PRIMARY KEY,
+  "user_id" uuid NOT NULL REFERENCES "USER"(id),
+  "skill_id" uuid NOT NULL REFERENCES "SKILL"(id),
+  "name" varchar(100) NOT NULL,
+  "description" text,
+  "start_at" timestamp NOT NULL,
+  "end_at" timestamp NOT NULL,
+  "max_participants" integer NOT NULL,
+  "meeting_url" varchar(2048) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE IF NOT EXISTS "CREDIT_PACKAGE" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(50) NOT NULL,
+  "cover_url" varchar(2048),
+  "credit_amount" integer NOT NULL,
+  "price" numeric(10,2) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
 -- 4-1. 新增：在`COURSE` 新增一門課程，資料需求如下：
     -- 1. 教練設定為用戶`李燕容` 
@@ -82,7 +312,17 @@
     -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
-
+INSERT INTO "COURSE" (user_id, skill_id, name, start_at, end_at, max_participants, meeting_url)
+VALUES
+  (
+    (SELECT id FROM "USER" WHERE name = '李燕容'),
+    (SELECT id FROM "SKILL" WHERE name = '重訓'),
+    '重訓基礎課',
+    '2024-11-25 14:00:00',
+    '2024-11-25 16:00:00',
+    10,
+    'https://test-meeting.test.io'
+  );
 
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
@@ -92,6 +332,19 @@
 -- ===================== ====================
 
 -- 5. 客戶預約與授課 COURSE_BOOKING
+CREATE TABLE "COURSE_BOOKING" (
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
+  "user_id" uuid NOT NULL REFERENCES "USER"(id),
+  "course_id" integer NOT NULL REFERENCES "COURSE"(id),
+  "booking_at" timestamp NOT NULL,
+  "status" varchar(20) NOT NULL,
+  "join_at" timestamp,
+  "leave_at" timestamp,
+  "cancelled_at" timestamp,
+  "cancellation_reason" varchar(255),
+  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
 -- 5-1. 新增：請在 `COURSE_BOOKING` 新增兩筆資料：
     -- 1. 第一筆：`王小明`預約 `李燕容` 的課程
         -- 1. 預約人設為`王小明`
@@ -101,23 +354,101 @@
         -- 1. 預約人設為 `好野人`
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
         -- 3. 狀態`status` 設定為即將授課
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+  (
+    (SELECT id FROM "USER" WHERE name = '王小明'),
+    (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE name = '李燕容')),
+    '2024-11-24 16:00:00',
+    '即將授課'
+  ),
+   (
+    (SELECT id FROM "USER" WHERE name = '好野人'),
+    (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE name = '李燕容')),
+    '2024-11-24 16:00:00',
+    '即將授課'
+  )
+;
+
+-- 優化版
+WITH
+  user_ids AS (
+    SELECT
+      (SELECT id FROM "USER" WHERE name = '王小明') AS wang_id,
+      (SELECT id FROM "USER" WHERE name = '好野人') AS hao_id,
+      (SELECT id FROM "USER" WHERE name = '李燕容') AS coach_id
+  ),
+  course_id AS (
+    SELECT id AS course_id
+    FROM "COURSE"
+    WHERE user_id = (SELECT coach_id FROM user_ids)
+  )
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+  ((SELECT wang_id FROM user_ids), (SELECT course_id FROM course_id), '2024-11-24 16:00:00', '即將授課'),
+  ((SELECT hao_id FROM user_ids), (SELECT course_id FROM course_id), '2024-11-24 16:00:00', '即將授課');
 
 -- 5-2. 修改：`王小明`取消預約 `李燕容` 的課程，請在`COURSE_BOOKING`更新該筆預約資料：
     -- 1. 取消預約時間`cancelled_at` 設為2024-11-24 17:00:00
     -- 2. 狀態`status` 設定為課程已取消
+UPDATE "COURSE_BOOKING"
+SET
+  cancelled_at = '2024-11-24 17:00:00',
+  status = '課程已取消'
+WHERE
+  user_id = (SELECT id FROM "USER" WHERE name = '王小明')
+  AND
+  course_id = (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE name = '李燕容'));
 
--- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
+-- 5-3. 新增：`王小明`再次預約 `李燕容` 的課程，請在`COURSE_BOOKING`新增一筆資料：
     -- 1. 預約人設為`王小明`
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
+WITH
+  user_ids AS (
+    SELECT 
+      (SELECT id FROM "USER" WHERE name = '王小明') AS wang_id,
+      (SELECT id FROM "USER" WHERE name = '李燕容') AS coach_id
+  ),
+  course_id AS (
+    SELECT id AS course_id
+    FROM "COURSE"
+    WHERE user_id = (SELECT coach_id FROM user_ids)
+  )
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+  (
+    (SELECT wang_id FROM user_ids),
+    (SELECT course_id FROM course_id),
+    '2024-11-24 17:10:25',
+    '即將授課'
+  );
 
 -- 5-4. 查詢：取得王小明所有的預約紀錄，包含取消預約的紀錄
+SELECT *
+FROM "COURSE_BOOKING"
+WHERE
+  user_id = (SELECT id FROM "USER" WHERE name = '王小明');
 
 -- 5-5. 修改：`王小明` 現在已經加入直播室了，請在`COURSE_BOOKING`更新該筆預約資料（請注意，不要更新到已經取消的紀錄）：
     -- 1. 請在該筆預約記錄他的加入直播室時間 `join_at` 設為2024-11-25 14:01:59
     -- 2. 狀態`status` 設定為上課中
+UPDATE "COURSE_BOOKING"
+SET
+  join_at = '2024-11-25 14:01:59',
+  status = '上課中'
+WHERE
+  user_id = (SELECT id FROM "USER" WHERE name = '王小明')
+  AND
+  cancelled_at IS NULL;
 
 -- 5-6. 查詢：計算用戶王小明的購買堂數，顯示須包含以下欄位： user_id , total。 (需使用到 SUM 函式與 Group By)
+SELECT user_id, SUM(purchased_credits) AS total
+FROM "CREDIT_PURCHASE"
+WHERE user_id = (
+    SELECT id FROM "USER" WHERE name = '王小明'
+)
+GROUP BY user_id;
 
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
 
